930505a3e6287a6155a15f9461e82e231fa7ee2d
EpicFate <76913940+EpicFate@users.noreply.github.com>
2021-02-16 16:51:09 +0300

    Update ApiConfigHelper.java

--- src/test/java/config/ApiConfigHelper.java
+++ src/test/java/config/ApiConfigHelper.java
@@ -4,13 +4,21 @@ import org.aeonbits.owner.ConfigFactory;
 
 public class ApiConfigHelper {
 
-    public static String getBody() { return getConfig().Body(); }
+    public static String getBody() { 
+        return getConfig().Body(); 
+    }
 
-    public static String getCookie() { return getConfig().Cookie(); }
+    public static String getCookie() {
+        return getConfig().Cookie(); 
+    }
 
-    public static String getContentType() { return getConfig().ContentType(); }
+    public static String getContentType() {
+        return getConfig().ContentType();
+    }
 
-    public static String getPost() { return getConfig().Post(); }
+    public static String getPost() {
+        return getConfig().Post(); 
+    }
 
     private static ApiConfig getConfig() {
         return ConfigFactory.newInstance().create(ApiConfig.class);
